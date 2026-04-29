3b219918c7332cfba61945e2924dc961713c8f67
Larissa Lapa <issaarl04@icloud.com>
2026-04-28 22:28:52 -0300

    config: firebase via variaveis de ambiente

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // apiKey: 'SUA_API_KEY',
-  // authDomain: 'SEU_AUTH_DOMAIN',
-  // projectId: 'SEU_PROJECT_ID',
-  // databaseURL: 'SUA_DATABASE_URL'
-  // storageBucket: 'SEU_STORAGE_BUCKET',
-  // messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  // appId: 'SEU_APP_ID',
-
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app);
+const db = getFirestore(app);
 
 export { auth, db };
